4eec76c6fad95f0094e1bc04677a08504b3ff2dd
Abdulkodir Yusupov <44323682+MustanGImmortal@users.noreply.github.com>
2020-08-22 19:04:41 +0500

    Add home page

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Job
 
-# Create your views here.
+def home_view(request):
+	jobs = Job.objects.all()
+	print(jobs)
+	ctx = {
+		"jobs": jobs
+	}
+	return render(request, 'jobs/home.html', ctx)
